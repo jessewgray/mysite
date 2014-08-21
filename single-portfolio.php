c2a5870d1ab5e8906ff1e32a359baa19cda994cc
jesse <jwgray2@gmail.com>
2014-08-21 15:05:07 -0400

    added a portfolio area

---
+++ single-portfolio.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="section">
+  <div class="innerWrapper">
+    <div class="full">
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+      	<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+		<?php the_field('short_desc'); ?>
+
+		<div class="images">
+			<?php while( has_sub_field('images') ): ?>
+
+				<div class="image">	
+					<p><?php the_sub_field('caption'); ?></p>
+					<?php $image = get_sub_field('image'); ?>
+					<!-- <pre><?php print_r($image);?></pre> -->
+  					<img src="<?php echo $image['sizes']['square'] ?>">
+				</div> <!-- image -->
+			<?php endwhile; ?>
+		</div> <!-- images -->
+
+		
+        
+      <?php endwhile; // end of the loop. ?>
+    </div>
+  </div> <!-- /.innerWrapper -->
+</div> <!-- /.section -->
+
+<?php get_footer(); ?>
